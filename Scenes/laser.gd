3b35ac4b345ef1f03b43081b466b4bbccc6ac721
Nathan Yu <nathanyu8th@gmail.com>
2026-01-09 21:15:37 -0800

    Finished artifact 1: Added animations for meteors, stars, and lasers. Added a starry background in the back

--- Scenes/laser.gd
+++ Scenes/laser.gd
@@ -4,9 +4,9 @@ extends Area2D
 
 func _ready():
 	
-	$Sprite2D.scale = Vector2(0,0)
+	
 	var tween = create_tween()
-	tween.tween_property($Sprite2D, "scale", Vector2(1,1), 0.1)
+	tween.tween_property($Sprite2D, "scale", Vector2(1,1), 0.2).from(Vector2(0,0))
 	
 	#timer to despawn
 	get_tree().create_timer(10.0).timeout.connect(queue_free)
