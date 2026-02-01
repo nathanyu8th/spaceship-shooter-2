dcb41e5b68d0bc514c65e0e778de2427f3a932cd
Nathan Yu <nathanyu8th@gmail.com>
2026-02-01 00:18:47 -0800

    SpaceShooter_15: Created a random position function for all the stars

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -3,7 +3,15 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://Scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
 
-
+func _ready():
+	#stars
+	var size = get_viewport().get_visible_rect().size
+	var rng = RandomNumberGenerator.new()
+	
+	for star in $Stars.get_children():
+		var random_x = rng.randi_range(0,size.x)
+		var random_y = rng.randi_range(0,size.y)
+		star.position = Vector2(random_x, random_y)
 
 func _on_meteor_timer_timeout() -> void:
 	var meteor = meteor_scene.instantiate()
