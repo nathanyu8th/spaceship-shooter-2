c00c4c89c210dfb976b5442b467af62f4f0d8ee4
Nathan Yu <nathanyu8th@gmail.com>
2026-02-08 23:37:42 -0800

    SpaceShooter_70: Finished Artifact 3

--- Scenes/hscore.gd
+++ Scenes/hscore.gd
@@ -3,10 +3,11 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "High Score: 0"
 	ScoreLayer.score_changed.connect(_on_high_score_changed)
+	_on_high_score_changed(ScoreLayer.high_score)
+	
 
 
 	
-func _on_score_changed(new_score: int):
-	text = "High Score: %d" % new_score
+func _on_high_score_changed(new_high_score: int):
+	text = "High Score: %d" % new_high_score
